d2c28de927940f916b1fe724888dbe95aa969573
kacety123 <krystal.thode@toadworld.org>
2024-09-08 17:05:35 -0400

    adding smol changes

--- main/WebApp/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ main/WebApp/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7e09cfdf34c0c9d00eaf625139b04863a07ae8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5349a7b606f6224a27dabb6c59c91079e5d86d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
