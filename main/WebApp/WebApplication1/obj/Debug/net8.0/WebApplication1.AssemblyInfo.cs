7302e6487fd06ed6bb03eff64ebc9d6402b064c7
kacety <krystal.thode@toadworld.org>
2024-09-09 19:38:53 -0400

    Added page and desired info to present pages. Ready for intial deployment to main

--- main/WebApp/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ main/WebApp/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0820a30d36fa3c010bfbe28e67bb6a318b6d103")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+412c313ce8848dba4497e7bce7b96ab29ea56280")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
