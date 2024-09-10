eda4ed8cbb3057ef67705ad918e490c311c0b1f1
kacety <krystal.thode@toadworld.org>
2024-09-09 20:22:08 -0400

    removing main folder from solution setup

--- WebApp/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApp/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+412c313ce8848dba4497e7bce7b96ab29ea56280")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81c65deebff64557da9cae2d84e80fab4ba8def8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
